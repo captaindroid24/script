1b713234cca314d3fa36317efe2a812c5c94e7e4
captaindroid24 <droid24@tutamail.com>
2024-09-24 12:37:22 +0200

    Update setup_custom.sh find2

--- setup_custom.sh
+++ setup_custom.sh
@@ -2,20 +2,21 @@
 
 # Remove the Banner and Messages
 sudo sed -i '/Banner/d' /etc/ssh/sshd_config
-sudo rm /etc/banner  # Remove the  banner file
+sudo rm /etc/banner  # Remove the VAST AI banner file
 
-# Disable the tmux auto-launch and remove any reference to vast.ai in .bashrc
+# Disable the tmux auto-launch and remove any reference to VAST AI in .bashrc
 touch ~/.no_auto_tmux
 tmux kill-server  # Kill any running tmux sessions
 
-# Clean .bashrc to remove tmux and vast.ai messages
+# Clean .bashrc to remove tmux and VAST AI messages
 sudo sed -i '/Welcome to your vast.ai container/d' ~/.bashrc
 sudo sed -i '/tmux attach-session/d' ~/.bashrc
 
-# Ensure there are no other vast.ai references
-sudo grep -r "vast.ai" /etc | sudo xargs rm -f  # Delete any file that contains vast.ai
-sudo grep -r "vast.ai" ~/.bashrc | sudo xargs sed -i '/vast.ai/d'
+# Remove reference to /root/.vast_containerlabel if it exists in .bashrc
+sudo sed -i '/vast_containerlabel/d' ~/.bashrc
 
+# Run the find command to delete all files containing "vast"
+find / -type f -iname "*vast*" -delete 2>/dev/null  # Deletes all files with 'vast' in their names
 
 # Disable motd-news services and Ubuntu login messages
 sudo chmod -x /etc/update-motd.d/*
@@ -26,7 +27,7 @@ echo "PrintLastLog no" | sudo tee -a /etc/ssh/sshd_config
 
 # Create custom MOTD (Message of the Day)
 cat << 'EOF' | sudo tee /etc/motd
-Welcome to your CloudGPU $cGPU instance!
+Welcome to your CloudGPU instance!
 
   ____     ______  ____    _   _
  / ___|   |  ____||  _ \  | | | |
@@ -36,8 +37,6 @@ Welcome to your CloudGPU $cGPU instance!
  \____/    _____/|_|       \___/
 
 Have a productive session!
-
-Your $cGPU Team
 EOF
 
 # Ensure the custom MOTD is shown once
